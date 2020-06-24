b97e9c30a2816c47f91f58f57994969ec693ab9f
Angel Flores <a_flores116@yahoo.com>
2020-06-23 22:28:08 -0500

    Adding terminology to the JS term game

---
+++ js/functions.js
@@ -0,0 +1,30 @@
+/*
+    Use this to help you out with JS terms out there.
+*/
+
+const jsTerms = {
+    var: 'what is a var?',
+    const: 'What is a const?',
+    let: 'what is a let?',
+    function: 'what is a function?',
+    closure: 'What is a closure?'
+};
+
+
+/*
+QUESTIONS:
+What is a var?
+A variable
+
+What is a const?
+A variable that cannot mutate
+
+What is a let?
+A variable that cannot be hoisted
+
+What is a function?
+A block of code that will be repeated
+
+What is a closure?
+When you stick a function inside a variable and at a later point in time, you have access to call any inner functions
+*/
